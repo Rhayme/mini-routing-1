26b86e5472af00380b95123259dd54200b4f8d46
lukeschunk <luke.schunk@mx.com>
2017-06-13 19:01:49 -0600

    commit

---
+++ js/controller.js
@@ -0,0 +1,3 @@
+// angular.module('miniRouting').controller('mainCtrl', function ($scope) {
+//
+// })
---
+++ js/home/homeCtrl.js
@@ -0,0 +1,3 @@
+angular.module('miniRouting').controller('homeCtrl', function($scope) {
+  $scope.homeTest = 'hey your home is working'
+})
